9b43a362c08d1ef47e8cccfc47df529c47f703b5
payalbnsl <jahanvi.bansal@gmail.com>
2018-11-30 01:31:14 +0530

    v1

---
+++ target/classes/Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:8/jdk
+EXPOSE 8080
+COPY target/demo.jar .
+CMD ["java" "-jar" "demo.jar"]
+
---
+++ target/classes/schema.sql
@@ -0,0 +1,9 @@
+
+create table employee 
+(
+id int not null,
+name varchar(50),
+salary decimal(20,2),
+emailId varchar(100)
+
+);
